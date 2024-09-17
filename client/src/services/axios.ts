2abb3877194bc6cfe946115da77c7b7660c8bf5f
Khoder <132526568+khoderdev@users.noreply.github.com>
2024-09-17 05:34:44 +0300

    Update axios.ts to use ngrok endpoints

--- client/src/services/axios.ts
+++ client/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://inventory-manager-backend-egws.onrender.com",
+  baseURL: "https://5987-185-76-176-30.ngrok-free.app",
 });
 
 export default api;
