feb0805c9e39b32b7dafc9ba5ff96482c58d315c
Khoder <132526568+khoderdev@users.noreply.github.com>
2024-09-17 05:47:39 +0300

    Update and rename .eslintrc.json to .eslintrc.js

---
+++ client/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  rules: {
+    "react/react-in-jsx-scope": "off", // Disable the rule
+    "@typescript-eslint/no-explicit-any": "off",
+    "react-hooks/exhaustive-deps": "off", // Turn off exhaustive-deps rule
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-non-null-assertion": "off", // Turn off non-null assertion rule
+  },
+};
