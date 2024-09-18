25867c1c969fe2f2b40ec00289a2ad4072dc1224
khoderdev <khoder.dev@gmail.com>
2024-09-18 10:34:25 +0300

    updated typography and terms v6

--- client/src/pages/Stores/index.tsx
+++ client/src/pages/Stores/index.tsx
@@ -126,7 +126,7 @@ const Stores = () => {
 
       {state.modalRegisters.openedModal &&
         <ModalRegisters
-          endpoint={state.modalRegisters.editingRegister ? 'store' : 'store'}
+          endpoint={state.modalRegisters.editingRegister ? 'stores' : 'stores'}
           type={state.modalRegisters.editingRegister ? 'update' : state.modalRegisters.deletingRegister ? 'delete' : 'create'}
           dataKey="store"
           title={state.modalRegisters.editingRegister ? 'Edit Unit' : state.modalRegisters.deletingRegister ? 'Delete Unit' : 'Add New Unit'}
