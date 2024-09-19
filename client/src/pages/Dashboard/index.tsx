5337dcd75e8b00f4177554328ad688655a547ec9
khoderdev <khoder.dev@gmail.com>
2024-09-19 08:36:50 +0300

    fixed dashboard roles access v1

--- client/src/pages/Dashboard/index.tsx
+++ client/src/pages/Dashboard/index.tsx
@@ -12,13 +12,13 @@ import * as C from './styles';
 const Dashboard = () => {
   const api = useApi();
   const auth = useContext(AuthContext);
-  const [data, setData] = useState<any>([]);
+  const [data, setData] = useState<any>(null); 
   const [targetDate, setTargetDate] = useState(7);
 
   const viewSalesAnalyticsRoles = ['Administration', 'Financial', 'Sales'];
   const viewProductsAnalyticsRoles = ['Administration', 'Sales', 'Deposit'];
-  const viewFinancialAnalyticsRoles = ['Administration', 'Financial'];
-  const viewStoresAnalyticsRoles = ['Administration'];
+  const viewFinancialAnalyticsRoles = ['Administration', 'Financial', 'Deposit'];
+  const viewStoresAnalyticsRoles = ['Administration', 'Deposit'];
 
   const fetchData = async () => {
     const result = await api.getData(targetDate);
@@ -26,7 +26,7 @@ const Dashboard = () => {
     if (result) {
       setData(result);
     }
-  }
+  };
 
   useEffect(() => {
     fetchData();
@@ -36,35 +36,41 @@ const Dashboard = () => {
   return (
     <C.Container>
       <C.Graphics>
-        {data.result &&
+        {data?.result && (
           <>
-            {viewSalesAnalyticsRoles.includes(auth.user?.position ?? '') &&
+            {viewSalesAnalyticsRoles.includes(auth.user?.position ?? '') && (
               <C.ContainerSections>
-                <C.TitleSections>Orders / Transfers in the last
+                <C.TitleSections>
+                  Orders / Transfers in the last
                   <C.BtnArea>
                     <button
                       className={targetDate === 7 ? 'active' : ''}
                       onClick={() => setTargetDate(7)}
-                    >07 days</button>
+                    >
+                      07 days
+                    </button>
                     <button
                       className={targetDate === 15 ? 'active' : ''}
                       onClick={() => setTargetDate(15)}
-                    >15 days</button>
+                    >
+                      15 days
+                    </button>
                     <button
                       className={targetDate === 30 ? 'active' : ''}
                       onClick={() => setTargetDate(30)}
-                    >30 days</button>
+                    >
+                      30 days
+                    </button>
                   </C.BtnArea>
                 </C.TitleSections>
                 <C.SalesArea>
                   <C.ResumeBalanceArea>
-                    <ResumeBalance value={data.result.sales.profit} type="Profit" />
-                    <ResumeBalance value={data.result.sales.received} type="Received" />
-                    {/* <ResumeBalance value={data.result.sales.invested} type="Invested" /> */}
+                    <ResumeBalance value={data.result?.sales?.profit ?? 0} type="Profit" />
+                    <ResumeBalance value={data.result?.sales?.received ?? 0} type="Received" />
                   </C.ResumeBalanceArea>
                   <C.SalesDaily>
                     <LineChartXR
-                      data={data.result.sales.list}
+                      data={data.result?.sales?.list ?? []}
                       dataKeyLine="quantitySales"
                       nameLine="Sales"
                       dataKeyXAxis="date"
@@ -72,30 +78,37 @@ const Dashboard = () => {
                   </C.SalesDaily>
                 </C.SalesArea>
               </C.ContainerSections>
-            }
+            )}
 
-            {viewStoresAnalyticsRoles.includes(auth.user?.position ?? '') &&
+            {viewStoresAnalyticsRoles.includes(auth.user?.position ?? '') && (
               <C.ContainerSections>
-                <C.TitleSections>Profit by store type in the last
+                <C.TitleSections>
+                  Profit by store type in the last
                   <C.BtnArea>
                     <button
                       className={targetDate === 7 ? 'active' : ''}
                       onClick={() => setTargetDate(7)}
-                    >07 days</button>
+                    >
+                      07 days
+                    </button>
                     <button
                       className={targetDate === 15 ? 'active' : ''}
                       onClick={() => setTargetDate(15)}
-                    >15 days</button>
+                    >
+                      15 days
+                    </button>
                     <button
                       className={targetDate === 30 ? 'active' : ''}
                       onClick={() => setTargetDate(30)}
-                    >30 days</button>
+                    >
+                      30 days
+                    </button>
                   </C.BtnArea>
                 </C.TitleSections>
                 <C.SalesArea>
                   <C.SalesDaily>
                     <PieChartXR
-                      data={data.result.stores.profitByType}
+                      data={data.result?.stores?.profitByType ?? []}
                       dataKey="profit"
                       nameKey="type"
                     />
@@ -106,7 +119,7 @@ const Dashboard = () => {
                         <C.TableLineItem>Top stores</C.TableLineItem>
                       </tr>
                     </C.TableLine>
-                    {data.result.stores.rankingByType.map((store: any, index: number) => (
+                    {data.result?.stores?.rankingByType?.map((store: any, index: number) => (
                       <C.TableContent key={index}>
                         <tr>
                           <C.TableContentItem>{store.name}</C.TableContentItem>
@@ -118,51 +131,58 @@ const Dashboard = () => {
                   </C.TableRanking>
                 </C.SalesArea>
               </C.ContainerSections>
-            }
+            )}
 
-            {viewProductsAnalyticsRoles.includes(auth.user?.position ?? '') &&
+            {viewProductsAnalyticsRoles.includes(auth.user?.position ?? '') && (
               <C.ContainerSections>
                 <C.TitleSections>Top 05 best-selling products</C.TitleSections>
                 <BarChartXR
-                  data={data.result.products.list}
+                  data={data.result?.products?.list ?? []}
                   dataKeyLine="sold_amount"
                   nameLine="Total sold"
                   dataKeyXAxis="name"
                 />
                 <C.AverageTicket>
                   <span>Average ticket: </span>
-                  <strong>{data.result.products.averageTotalTicket}</strong>
+                  <strong>{data.result?.products?.averageTotalTicket ?? 'N/A'}</strong>
                 </C.AverageTicket>
               </C.ContainerSections>
-            }
+            )}
 
-            {viewFinancialAnalyticsRoles.includes(auth.user?.position ?? '') &&
+            {viewFinancialAnalyticsRoles.includes(auth.user?.position ?? '') && (
               <C.ContainerSections>
-                <C.TitleSections>Manual entries in the last
+                <C.TitleSections>
+                  Manual entries in the last
                   <C.BtnArea>
                     <button
                       className={targetDate === 7 ? 'active' : ''}
                       onClick={() => setTargetDate(7)}
-                    >07 days</button>
+                    >
+                      07 days
+                    </button>
                     <button
                       className={targetDate === 15 ? 'active' : ''}
                       onClick={() => setTargetDate(15)}
-                    >15 days</button>
+                    >
+                      15 days
+                    </button>
                     <button
                       className={targetDate === 30 ? 'active' : ''}
                       onClick={() => setTargetDate(30)}
-                    >30 days</button>
+                    >
+                      30 days
+                    </button>
                   </C.BtnArea>
                 </C.TitleSections>
                 <C.SalesArea>
                   <C.ResumeBalanceArea>
-                    <ResumeBalance value={data.result.financial.difference} type="Balance" />
-                    <ResumeBalance value={data.result.financial.totalEntries} type="Entry" />
-                    <ResumeBalance value={data.result.financial.totalOutputs} type="Exit" />
+                    <ResumeBalance value={data.result?.financial?.difference ?? 0} type="Balance" />
+                    <ResumeBalance value={data.result?.financial?.totalEntries ?? 0} type="Entry" />
+                    <ResumeBalance value={data.result?.financial?.totalOutputs ?? 0} type="Exit" />
                   </C.ResumeBalanceArea>
                   <C.SalesDaily>
                     <BarChartPositiveAndNegativeXR
-                      data={data.result.financial.list}
+                      data={data.result?.financial?.list ?? []}
                       dataKeyPositive="positive"
                       datasKeyNegative="negative"
                       nameLinePositive="Entry"
@@ -172,29 +192,33 @@ const Dashboard = () => {
                   </C.SalesDaily>
                 </C.SalesArea>
               </C.ContainerSections>
-            }
+            )}
           </>
-        }
+        )}
       </C.Graphics>
 
-      {viewSalesAnalyticsRoles.includes(auth.user?.position ?? '') &&
+      {viewSalesAnalyticsRoles.includes(auth.user?.position ?? '') && (
         <C.LastSalesArea>
           <C.TitleLastSales>Last Orders / Transfers</C.TitleLastSales>
-          {data.result &&
+          {data?.result?.sales?.lastSales?.length > 0 ? (
             <C.LastSalesAreaCards>
-              {data.result.sales.lastSales.length > 0 ? (
-                data.result.sales.lastSales.map((item: any, index: number) => (
-                  <LastSale key={index} price={item.price} name={item.name} unity={item.unity} quantity={item.quantity} />
-                ))
-              ) : (
-                <p style={{ fontSize: '12px' }}>No records.</p>
-              )
-              }
-            </C.LastSalesAreaCards>}
+              {data.result?.sales?.lastSales.map((item: any, index: number) => (
+                <LastSale
+                  key={index}
+                  price={item.price}
+                  name={item.name}
+                  unity={item.unity}
+                  quantity={item.quantity}
+                />
+              ))}
+            </C.LastSalesAreaCards>
+          ) : (
+            <p style={{ fontSize: '12px' }}>No records.</p>
+          )}
         </C.LastSalesArea>
-      }
+      )}
     </C.Container>
-  )
-}
+  );
+};
 
 export default Dashboard;
